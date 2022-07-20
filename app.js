1a04de8d6bd5a9fd9dab53bac67e54e9ae26a2da
Tyler Wellman <TangoKilo1985@gmail.com>
2022-07-20 07:55:18 -0400

    Gitz It

---
+++ app.js
@@ -0,0 +1,19 @@
+//Falsy Bouncer
+
+function bouncer(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) newArray.push(arr[i]);
+    }
+    return newArray;
+  }
+
+//Chunky Monkey
+
+  function chunkArrayInGroups(arr, size) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+      newArr.push(arr.slice(i, i + size));
+    }
+    return newArr;
+  }
